b5659da2c342137098d591970ceba0bf0e085fca
Kyenshak Dabit <kendabit@gmail.com>
2022-10-13 17:15:30 -0700

    4 corr

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Description: A C programme that a string usinf puts function
+ * Description: A C programme that a string using puts function
  *
  * Return: Always 0 (success)
  */

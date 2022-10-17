8a86401d3cf32ef213a03ba80bcae8eb30fa319b
Kyenshak Dabit <kendabit@gmail.com>
2022-10-17 04:28:37 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,23 +10,19 @@
 */
 
 int main(void)
-{
-	int digit = 0;
 
+{
+		int digit = 0;
 	while (digit <= 9)
 	{
 		putchar(digit + 48);
-
 		if (digit != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-
 		++digit;
 	}
-
 	putchar('\n');
-	
 	return (0);
 }

7a3fbd2092bc9ce6202b464f4e97c56ee2536f05
Kyenshak Dabit <kendabit@gmail.com>
2022-10-27 21:58:53 -0700

    pointers correction

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,19 +6,29 @@
   *
   * Return: char var
   */
+
 char *string_toupper(char *p)
+
 {
+
 	int a = 0;
 
 	while (p[a])
+
 	{
+
 		if (p[a] >= 97 && p[a] <= 122)
+
 		{
+
 			p[a] -= 32;
+
 		}
 
+
 		a++;
+
 	}
 
 	return (p);
-
+}
